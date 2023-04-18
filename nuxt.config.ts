e30d302c4028b3072e24395edf1ac8431429e9ec
mohirbeck <mustaqimovohir@gmail.com>
2023-04-18 15:27:05 +0500

    sliders

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        // SCSS file in the project
-        "~/assets/style/main.scss", // you should add main.scss somewhere in your app
-      ],
-      postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-      build: {
-        transpile: ["@heroicons/vue"],
-      },
+  css: [
+    // SCSS file in the project
+    "~/assets/style/main.scss", // you should add main.scss somewhere in your app
+  ],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  build: {
+    transpile: ["@heroicons/vue"],
+  },
+  runtimeConfig: {
+    public: {
+      apiBase: process.env.NUXT_API_BASE_URL || "",
+      baseUrl: process.env.NUXT_BASE_URL || "",
+    }
+  }
 })
--- pages/works/[slug].vue
+++ pages/works/[slug].vue
@@ -50,9 +50,161 @@
         </p>
       </div>
     </div>
+    <div class="pl-[18px] lg:pr-[18px]">
+      <div class="container mx-auto">
+        <div class="flex items-center w-full md:mb-[52px] mb-8 justify-between">
+          <h3
+            class="xl:text-5xl md:text-4xl text-2xl text-center font-semibold"
+          >
+            Похожие проекты
+          </h3>
+          <div class="flex items-center space-x-5" v-if="false">
+            <button
+              class="rounded-full bg-primary flex items-center justify-center p-1"
+              @click="prev"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                class="w-5 h-5 stroke-white"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 19.5L8.25 12l7.5-7.5"
+                />
+              </svg>
+            </button>
+            <button
+              class="rounded-full bg-primary flex items-center justify-center p-1"
+              @click="next"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                class="w-5 h-5 stroke-white"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+        <swiper
+          :slides-per-view="4"
+          :space-between="18"
+          @swiper="onSwiper"
+          @slideChange="onSlideChange"
+          :modules="modules"
+          :pagination="{
+            el: '.swiper-pagination',
+            clickable: true,
+            renderBullet: (index, className) => {
+              return `<span class='${className}'></span>`;
+            },
+          }"
+          :breakpoints="{
+            320: {
+              slidesPerView: 'auto',
+              spaceBetween: 12,
+            },
+            768: {
+              slidesPerView: 'auto',
+              spaceBetween: 18,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 23,
+            },
+          }"
+        >
+          <swiper-slide
+            v-for="(work, i) in work.similar_projects"
+            :key="i"
+            class="slide"
+          >
+            <WorkCard />
+          </swiper-slide>
+          <div class="mt-3 w-full">
+            <div class="swiper-pagination pagination"></div>
+          </div>
+        </swiper>
+      </div>
+    </div>
   </section>
 </template>
 <script>
-export default {};
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  data() {
+    return {
+      swiper: null,
+      activeIndex: 0,
+      modules: [Pagination],
+    };
+  },
+  methods: {
+    onSwiper(swiper) {
+      this.swiper = swiper;
+    },
+    onSlideChange() {
+      this.activeIndex = this.swiper.activeIndex;
+    },
+    next() {
+      this.swiper.slideNext();
+    },
+    prev() {
+      this.swiper.slidePrev();
+    },
+  },
+  computed: {
+    mobile() {
+      if (process.client) {
+        return window.innerWidth < 768;
+      }
+      return false;
+    },
+    locale() {
+      let storageLocale;
+      if (process.client) {
+        storageLocale = localStorage.getItem("locale")
+          ? JSON.parse(localStorage.getItem("locale"))
+          : undefined;
+      }
+      return storageLocale || { name: "Русский", alpha: "ru" };
+    },
+  },
+};
 </script>
-<style lang=""></style>
+<script setup>
+import WorkCard from "@/components/works/WorkCard.vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+const { data: work } = await apiRequest(
+  `projects/${router.currentRoute.value.params.slug}`
+);
+</script>
+
+<style lang="scss" scoped>
+.slide {
+  @media screen and (max-width: 768px) {
+    width: 146px !important;
+  }
+}
+</style>
